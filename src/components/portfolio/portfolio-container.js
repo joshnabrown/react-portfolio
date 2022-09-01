446c087c5974301c9d82f32141b60679c995f97e
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-09-01 12:02:11 -0400

    completed styles for Home page of portfolio items.

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -65,20 +65,14 @@ export default class PortfolioContainer extends Component {
             return <div>Loading... </div>;
         }
         return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+            <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button className="btn" onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button className="btn" onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
 
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-                <div className="portfolio-items-wrapper">
-                    {this.portfolioItems()}
-                </div>
-                {/* <hr />
-
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button> */}
+                {this.portfolioItems()}
             </div>
+
         )
     }
 }
