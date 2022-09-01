349886cbfc221ca1fb339a86991d1d0bfcdc477d
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-09-01 14:01:48 -0400

    added the basics of the auth form

--- src/components/app.js
+++ src/components/app.js
@@ -8,12 +8,13 @@ import {
 
 // import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
-import Blog from './pages/blog'
-import PortfolioDetail from './portfolio/portfolio-detail'
-import NoMatch from './pages/no-match'
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
+import PortfolioDetail from './portfolio/portfolio-detail';
+import Auth from './pages/auth';
+import NoMatch from './pages/no-match';
 
 
 
@@ -30,6 +31,7 @@ export default class App extends Component {
 
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route path="/auth" component={Auth} />
               <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
