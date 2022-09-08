9ca17a31c49fc628d494073a0f2c0e59039bc69d
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-09-08 17:44:03 -0400

    almost done with Blog edit function

--- src/components/motals/blog-modal.js
+++ src/components/motals/blog-modal.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
+import BlogForm from "../blog/blog-form";
+
+
+
+ReactModal.setAppElement(".app-wrapper");
 
 export default class BlogModal extends Component {
     constructor() {
         super();
 
+        this.customStyles = {
+            content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%",
+                width: "800px"
+
+            },
+            overlay: {
+                backgroundColor: "rgba(1, 1, 1, 0.75)"
+            }
+
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
+    }
+
+    handleSuccessfulFormSubmission(blog) {
+        this.props.handleSuccessfulNewBlogSubmission(blog)
+
     }
 
     render() {
         return (
-            <ReactModal isOpen={true}>
-                <h1>I'm in a modal!</h1>
+            <ReactModal
+                style={this.customStyles}
+                onRequestClose={() => {
+                    this.props.handleModalClose()
+                }}
+                isOpen={this.props.modalIsOpen} >
+                <BlogForm handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission} />
             </ReactModal>
         )
     }
