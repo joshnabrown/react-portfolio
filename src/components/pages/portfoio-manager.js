db05619a2d07154015dcb25c18004606b05a7dcc
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-09-08 13:05:42 -0400

    Added Infinate Scrolling to Blog

--- src/components/pages/portfoio-manager.js
+++ src/components/pages/portfoio-manager.js
@@ -9,14 +9,55 @@ export default class PortfolioManager extends Component {
         super();
 
         this.state = {
-            portfolioItems: []
+            portfolioItems: [],
+            portfolioToEdit: {},
         }
 
-        this.handleSuccessFormSubmission = this.handleSuccessFormSubmission.bind(this)
+        this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this)
+        this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this)
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this)
+        this.handleEditClick = this.handleEditClick.bind(this)
+        this.clearPortfolioToEdit = this.clearPortfolioToEdit.bind(this);
     }
 
-    handleSuccessFormSubmission(portfolioItem) {
+    clearPortfolioToEdit() {
+        this.setState({
+            portfolioToEdit: {}
+        });
+    }
+
+    handleEditClick(portfolioItem) {
+        this.setState({
+            portfolioToEdit: portfolioItem
+        });
+    }
+
+    handleDeleteClick(portfolioItem) {
+        axios
+            .delete(
+                `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+                { withCredentials: true })
+            .then(response => {
+                this.setState({
+                    portfolioItems: this.state.portfolioItems.filter(item => {
+                        return item.id !== portfolioItem.id;
+                    })
+                })
+
+                return response.data;
+            })
+            .catch(error => {
+                console.log("handleDeleteClick", error)
+
+            })
+    }
+
+    handleEditFormSubmission() {
+        this.getPortfolioItems();
+    }
+
+    handleNewFormSubmission(portfolioItem) {
         console.log("handleSuccessfulFormSubmission", portfolioItem);
         this.setState({
             portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
@@ -51,12 +92,20 @@ export default class PortfolioManager extends Component {
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
                     <PortfolioForm
-                        handleSuccessFormSubmission={this.handleSuccessFormSubmission}
-                        handleFormSubmissionError={this.handleFormSubmissionError} />
+                        handleNewFormSubmission={this.handleNewFormSubmission}
+                        handleEditFormSubmission={this.handleEditFormSubmission}
+                        handleFormSubmissionError={this.handleFormSubmissionError}
+                        clearPortfolioToEdit={this.clearPortfolioToEdit}
+                        portfolioToEdit={this.state.portfolioToEdit}
+                    />
                 </div>
 
                 <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems} />
+                    <PortfolioSidebarList
+                        handleDeleteClick={this.handleDeleteClick}
+                        data={this.state.portfolioItems}
+                        handleEditClick={this.handleEditClick}
+                    />
                 </div>
             </div>
         );
