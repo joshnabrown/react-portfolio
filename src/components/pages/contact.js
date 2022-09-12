e539b29b6e6180dd74478ca195ba89a30d634215
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-09-12 17:20:43 -0400

    added about page and contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
 export default function () {
     return (
-        <div>
-            Contact
+        <div className="content-page-wrapper">
+            <div
+                className="left-column"
+                style={{
+                    background: "url(" + contactPagePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            />
+            <div className="right-column">
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="phone" />
+                        </div>
+                        <div className="text">
+                            919-671-3000
+                        </div>
+                    </div>
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="envelope" />
+                        </div>
+                        <div className="text">
+                            joshnabrown@gmail.com
+                        </div>
+                    </div>
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="map-marked-alt" />
+                        </div>
+                        <div className="text">
+                            Fuquay-Varina, NC
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
